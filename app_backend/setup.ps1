9cb446dcabd7e733714e2186c9dd55722d8ab916
larsfop <lars.opgaard@hotmail.com>
2026-02-10 10:34:57 +0100

    removed some files

--- app_backend/setup.ps1
+++
@@ -1,5 +0,0 @@
-
-python3 dropbox_oauth.py
-
-
-start-process "http://localhost:8000/recreate-database"
--- app_backend/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Setup backend environment
-docker compose build
-docker compose up
-
-# Recreate database
-xdg-open http://localhost:4000/recreate-database
