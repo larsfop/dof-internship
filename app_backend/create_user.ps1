c0fdbaf9dc397667b74631fd6448c65028d29812
larsfop <lars.opgaard@hotmail.com>
2026-04-15 16:21:45 +0200

    infitesimal changes

--- app_backend/create_user.ps1
+++
@@ -1,6 +0,0 @@
-param(
-    [string(Mandatory=$true, HelpMessage="Enter the username for the new user", Position=0)]$username,
-    [string(Mandatory=$true, HelpMessage="Enter the password for the new user", Position=1)]$password
-)
-
-docker exec -it backend python3 src/create_user.py -u $username -p $password
--- app_backend/src/gunicorn.conf.py
+++ app_backend/src/gunicorn.conf.py
@@ -1,9 +1,13 @@
 bind = "0.0.0.0:8015"
 workers = 1
 worker_class = "uvicorn.workers.UvicornWorker"
-pidfile = "gunicorn.pid"
+pidfile = "../data/gunicorn.pid"
 
 timeout = 300
 loglevel = "info"
 # reload = True
 # reload_engine = 'poll'
+
+def post_worker_init(worker):
+    from main import app_process_all_pdfs
+    app_process_all_pdfs()
